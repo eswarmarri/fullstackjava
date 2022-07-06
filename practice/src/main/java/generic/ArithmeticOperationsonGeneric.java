ebf7d1bfbcb010c5a069e7bd9de6fbe842c61031
eswar <eswar@Eswar>
2022-07-06 16:52:20 +0530

    +Generic

---
+++ practice/src/main/java/generic/ArithmeticOperationsonGeneric.java
@@ -0,0 +1,8 @@
+package generic;
+
+import org.hamcrest.core.IsInstanceOf;
+
+public class ArithmeticOperationsonGeneric <G extends Number>
+{
+	
+}
--- practice/src/main/java/practice/practice.java
+++ practice/src/main/java/practice/practice.java
@@ -1,24 +1,19 @@
 package practice;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
-import iostreampackage.ReadandWrite;
-import iostreampackage.copystream;
-import threads.*;
-import threads.seperate.Randomandfact;
-import threads.seperate.Threadforfact;
-import threads.seperate.Threadforrandom;
 public class practice {
 	public static Integer a;
 
 	public static void main(String[] args) {
-		ReadandWrite raw = new ReadandWrite();
-		try {
-			raw.randomwrite();
-			raw.readrandom();
-			(new copystream()).copywithoutspaces();
-		} catch (IOException e) {
-			e.printStackTrace();
+		ArrayList<Integer> al = new ArrayList<>();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		Iterator<Integer> it = al.iterator();
+		it.next();
+		it.next();
+		it.remove();
 		}
-		
-	}
 }
